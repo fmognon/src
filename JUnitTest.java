3e34225e175e84509ca8ec1b0c3a2d57ca9cfd25
Fernando Mognon <fmognon@gmail.com>
2015-10-28 21:08:57 -0200

    ProjetoMetodsAgeis
    MetodosAgeis

--- JUnitTest.java
+++ JUnitTest.java
@@ -10,5 +10,146 @@ public class JUnitTest {
 		int result = junit.add(100,200);
 		assertEquals(300, result);
 	}
+	
 
+	@Test
+	public void testZonaMaffetoneMin() {
+		myJunitClass junit = new myJunitClass();
+		int resultMin = junit.calcularMaffetoneMin(60);
+		assertEquals(110, resultMin);
+	}
+
+	@Test
+	public void testZonaMaffetoneMax() {
+		myJunitClass junit = new myJunitClass();
+		int resultMax = junit.calcularMaffetoneMax(60);
+		assertEquals(120, resultMax);
+	}
+	
+	@Test
+	public void testZonaMaffetoneMinCasoA() {
+		myJunitClass junit = new myJunitClass();
+		int resultMin = junit.calcularMaffetoneMinCasoA(60);
+		assertEquals(100, resultMin);
+	}
+
+	@Test
+	public void testZonaMaffetoneMaxCasoA() {
+		myJunitClass junit = new myJunitClass();
+		int resultMax = junit.calcularMaffetoneMaxCasoA(60);
+		assertEquals(110, resultMax);
+	}
+	
+	@Test
+	public void testZonaMaffetoneMinCasoB() {
+		myJunitClass junit = new myJunitClass();
+		int resultMin = junit.calcularMaffetoneMinCasoB(60);
+		assertEquals(105, resultMin);
+	}
+
+	@Test
+	public void testZonaMaffetoneMaxCasoB() {
+		myJunitClass junit = new myJunitClass();
+		int resultMax = junit.calcularMaffetoneMaxCasoB(60);
+		assertEquals(115, resultMax);
+	}
+	
+	@Test
+	public void testFrielZona1() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielZona1(200);
+		assertEquals(170, result, 0);
+	}
+	
+	@Test
+	public void testFrielZona2() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielZona2(200);
+		assertEquals(178, result, 0);
+	}
+	
+	@Test
+	public void testFrielZona3() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielZona3(200);
+		assertEquals(188, result, 0);
+	}
+	
+	@Test
+	public void testFrielZona4() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielZona4(200);
+		assertEquals(198, result, 0);
+	}
+	
+	@Test
+	public void testFrielZona5a() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielZona5a(200);
+		assertEquals(204, result, 0);
+	}
+	
+	@Test
+	public void testFrielZona5b() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielZona5b(200);
+		assertEquals(212, result, 0);
+	}
+	
+	@Test
+	public void testFrielZona5c() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielZona5c(200);
+		assertEquals(212, result, 0);
+	}
+	
+	//bike
+	@Test
+	public void testFrielBikeZona1() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielBikeZona1(200);
+		assertEquals(162, result, 0);
+	}
+	
+	@Test
+	public void testFrielBikeZona2() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielBikeZona2(200);
+		assertEquals(178, result, 0);
+	}
+	
+	@Test
+	public void testFrielBikeZona3() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielBikeZona3(200);
+		assertEquals(186, result, 0);
+	}
+	
+	@Test
+	public void testFrielBikeZona4() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielBikeZona4(200);
+		assertEquals(198, result, 0);
+	}
+	
+	@Test
+	public void testFrielBikeZona5a() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielBikeZona5a(200);
+		assertEquals(204, result, 0);
+	}
+	
+	@Test
+	public void testFrielBikeZona5b() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielBikeZona5b(200);
+		assertEquals(212, result, 0);
+	}
+	
+	@Test
+	public void testFrielBikeZona5c() {
+		myJunitClass junit = new myJunitClass();
+		double result = junit.calcularFrielBikeZona5c(200);
+		assertEquals(212, result, 0);
+	}
 }
--- myJunitClass.java
+++ myJunitClass.java
@@ -7,4 +7,81 @@ public class myJunitClass {
 		return a + b;
 		
 	}
+
+	public int calcularMaffetoneMax(int i) {
+		return 180 - i;
+	}
+	public int calcularMaffetoneMin(int i) {
+		return  calcularMaffetoneMax(i) - 10;
+	}
+	
+	public int calcularMaffetoneMaxCasoA(int i) {
+		return 180 - i -10;
+	}
+	public int calcularMaffetoneMinCasoA(int i) {
+		return  calcularMaffetoneMaxCasoA(i) - 10;
+	}
+	
+	public int calcularMaffetoneMaxCasoB(int i) {
+		return 180 - i -5;
+	}
+	public int calcularMaffetoneMinCasoB(int i) {
+		return  calcularMaffetoneMaxCasoB(i) - 10;
+	}
+
+	public double calcularFrielZona1(int i) {
+		return (i * 0.85);
+	}
+	
+	public double calcularFrielZona2(int i) {
+		return (i * 0.89);
+	}
+	
+	public double calcularFrielZona3(int i) {
+		return (i * 0.94);
+	}
+	
+	public double calcularFrielZona4(int i) {
+		return (i * 0.99);
+	}
+	
+	public double calcularFrielZona5a(int i) {
+		return (i * 1.02);
+	}
+	
+	public double calcularFrielZona5b(int i) {
+		return (i * 1.06);
+	}
+	
+	public double calcularFrielZona5c(int i) {
+		return (i * 1.06);
+	}
+	//bike
+	public double calcularFrielBikeZona1(int i) {
+		return (i * 0.81);
+	}
+	
+	public double calcularFrielBikeZona2(int i) {
+		return (i * 0.89);
+	}
+	
+	public double calcularFrielBikeZona3(int i) {
+		return (i * 0.93);
+	}
+	
+	public double calcularFrielBikeZona4(int i) {
+		return (i * 0.99);
+	}
+	
+	public double calcularFrielBikeZona5a(int i) {
+		return (i * 1.02);
+	}
+	
+	public double calcularFrielBikeZona5b(int i) {
+		return (i * 1.06);
+	}
+	
+	public double calcularFrielBikeZona5c(int i) {
+		return (i * 1.06);
+	}
 }
